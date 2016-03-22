0de24ad83057c9cd2f81bee006af78cd569315ad
Fabian Sinz <sinz@bcm.edu>
2016-03-22 18:34:08 -0500

    intermediate commit

--- python/cortical_map/reconstruction.py
+++ python/cortical_map/reconstruction.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 from cortical_map.utils import rasterize, plot_cells, spin_shuffle, compute_overlap_density
 from . import connectivity as conn
 from itertools import product
+import seaborn as sns
+import pandas as pd
+from .utils import extended_hinton, plot_connections, load_data
 
 schema = dj.schema('shan_reconstruction', locals())
 
@@ -41,9 +44,59 @@ class DensityParameters(dj.Lookup):
     """
 
     contents = [
-        dict(density_param_id=0, bin_width=15, raster_resolution=.5, cell_offset=0, multiplication_factor=2,
+        dict(density_param_id=0, bin_width=15, raster_resolution=.5, cell_offset=0, multiplication_factor=1,
+             cut_compensation=1),
+        dict(density_param_id=1, bin_width=15, raster_resolution=.5, cell_offset=0, multiplication_factor=10,
+             cut_compensation=1),
+        dict(density_param_id=2, bin_width=15, raster_resolution=.5, cell_offset=50, multiplication_factor=10,
              cut_compensation=1)
     ]
+
+
+@schema
+class CellNameTranslation(dj.Lookup):
+    definition = """
+    shan        : char(12)
+    xiaolong    : char(12)
+    paper       : char(12)
+    """
+
+    contents = [('L1 eNGC', 'L1 eNGC', 'L1 eNGC'),
+                ('L1 SBC', 'L1 non-eNGC', 'L1 SBC-like'),
+                ('L23 BC', 'L23 BC', 'L23 BC'),
+                ('L23 BPC', 'L23 BPC', 'L23 BPC'),
+                ('L23 BTC', 'L23 BTC', 'L23 BTC'),
+                ('L23 ChC', 'L23 ChC', 'L23 ChC'),
+                ('L23 DBC', 'L23 DBC', 'L23 DBC'),
+                ('L23 MaC', 'L23 MC', 'L23 MC'),
+                ('L23 NGC', 'L23 NGC', 'L23 NGC'),
+                ('L23 Pyr', 'L23 Pyr', 'L23 Pyr'),
+                ('L5 BC', 'L5 BC', 'L5 BC'),
+                ('L5 DC', 'L5 DC', 'L5 DC'),
+                ('L5 HEC', 'L5 HEC', 'L5 HEC'),
+                ('L5 MaC', 'L5 MC', 'L5 MC'),
+                ('L5 NGC', 'L5 NGC', 'L5 NGC'),
+                ('L5 Pyr', 'L5 Pyr', 'L5 Pyr'),
+                ('L5 SC', 'L5 Shrub', 'L5 SC')]
+
+
+@schema
+class PaperConnectivity(dj.Lookup):
+    definition = """
+    cell_type_from      : varchar(256)
+    cell_type_to        : varchar(256)
+    ---
+    k       : int
+    n       : int
+    p       : float
+    """
+
+    @property
+    def contents(self):
+        labels, layers, K, N = load_data(transpose=False)
+        for (i, post), (j, pre) in product(enumerate(labels), enumerate(labels)):
+            yield dict(cell_type_from=pre, cell_type_to=post, k=K[i, j], n=N[i, j], p=K[i, j] / N[i, j])
+
 
 @schema
 class CellTypePair(dj.Lookup):
@@ -58,7 +111,8 @@ class CellTypePair(dj.Lookup):
     @property
     def contents(self):
         ct = CellType().fetch['cell_type_name']
-        yield from product(ct,ct)
+        yield from product(ct, ct)
+
 
 @schema
 class OverlapGroup(dj.Computed):
@@ -79,7 +133,6 @@ class OverlapGroup(dj.Computed):
         p           : longblob # overlap density along that line
         """
 
-
     def _make_tuples(self, key):
         # get identifiers for different regions
         AXON = (CellRegion() & dict(cell_region_name='axon')).fetch1['cell_region_id']
@@ -87,60 +140,51 @@ class OverlapGroup(dj.Computed):
 
         # get parameters for density estimation
         raster_resolution, bin_width, cell_offset, multiplication_factor, cut_compensation = \
-            (DensityParameters() & key).fetch1['raster_resolution','bin_width',
-                                               'cell_offset','multiplication_factor', 'cut_compensation']
+            (DensityParameters() & key).fetch1['raster_resolution', 'bin_width',
+                                               'cell_offset', 'multiplication_factor', 'cut_compensation']
 
         # load all cells from the particular pair of cell types
         X, region, cells = {}, {}, {}
-        for role in ('from','to'):
-            try:
-                layer, morph = (CellType() & dict(cell_type_name=key['cell_type_' + role])).fetch1['layer', 'cell_type_morph']
-            except:
-                #----------------------------------
-                # TODO: Remove this later
-                from IPython import embed
-                embed()
-                exit()
-                #----------------------------------
+        for role in ('from', 'to'):
+            layer, morph = (CellType() & dict(cell_type_name=key['cell_type_' + role])).fetch1[
+                'layer', 'cell_type_morph']
 
             cells[role] = (conn.ConnectMembership() * conn.Cell()
-                          & dict(role=role, cell_layer=layer, cell_type_morph=morph)
-                          ).project(**{('cell_id_' +role):'cell_id'})
+                           & dict(role=role, cell_layer=layer, cell_type_morph=morph)
+                           ).project(**{('cell_id_' + role): 'cell_id'})
             X[role], region[role] = OverlapGroup.load_cells(Tree() * CellType()
                                                             & dict(layer=layer, cell_type_morph=morph),
-                    resolution=raster_resolution, correct_cut=cut_compensation)
-
+                                                            resolution=raster_resolution, correct_cut=cut_compensation)
 
         # when there are no distances
         if not conn.Distance() * cells['from'] * cells['to']:
-            self.insert1(key)
+            # self.insert1(key)
             return
-        
+
         # get deltas
         keys, delta_x, delta_y = (conn.Distance() * cells['from'] * cells['to']).fetch[dj.key, 'delta_x', 'delta_y']
         offsets = np.c_[delta_x, -cell_offset * np.ones_like(delta_x), delta_y]
 
-        
         # save a control figure
-        fig, ax = plot_cells(X['from'], X['to'], offsets[0], dict(color='silver', label=key['cell_type_from']),
-                             dict(color='skyblue', label=key['cell_type_to']))
+        fig, ax = plot_cells(X['from'], X['to'], offsets[0], dict(color=sns.xkcd_rgb['neon pink'], label=key['cell_type_from']),
+                             dict(color=sns.xkcd_rgb['neon blue'], label=key['cell_type_to']))
         fig.savefig('{cell_type_from}_to_{cell_type_to}.png'.format(**key))
-
         plt.close(fig)
+
         # shuffle by rotation around z-axis and increase data volume
         X['from'] = spin_shuffle(X['from'], copy=multiplication_factor)
         X['to'] = spin_shuffle(X['to'], copy=multiplication_factor)
         for role in region:
             region[role] = np.tile(region[role], multiplication_factor)
-            
+
         # insert grouping key
         self.insert1(key)
         part = OverlapGroup.OrthoOverlapDensity()
 
         # insert an element in part (sub) table for each distance between cells of that type
         for k, delta in zip(keys, offsets):
-
-            H, E = compute_overlap_density(X['from'][region['from'] == AXON], X['to'][region['to'] == DENDRITE], bin_width, delta)
+            H, E = compute_overlap_density(X['from'][region['from'] == AXON], X['to'][region['to'] == DENDRITE],
+                                           bin_width, delta)
 
             # compute bin centers along y-axis and only get positive side
             y = E[1]
@@ -148,14 +192,13 @@ class OverlapGroup(dj.Computed):
             idx = y > 0
 
             # marginalize density
-            p = H[:,idx,:].sum(axis=(0,2))
-            key.update({f:k[f] for f in k.dtype.fields})
+            p = H[:, idx, :].sum(axis=(0, 2))
+            key.update({f: k[f] for f in k.dtype.fields})
             key.pop('cell_id_from')
             key.pop('cell_id_to')
             key['p'] = p
             key['d'] = y[idx]
             part.insert1(key)
-
 
     @staticmethod
     def load_cells(trees, resolution=.5, correct_cut=False):
@@ -184,16 +227,61 @@ class OverlapGroup(dj.Computed):
             mu = np.mean(node_coords[region == CELLBODY, :], axis=0)
             node_coords -= mu
 
-
             X, region = rasterize(node_coords, connected_pairs, region, resolution)
             if correct_cut:
-                ymax = X[:,1].max()
-                idx = X[:,1] <= -ymax
-                tmp  = X[idx, :]
-                tmp[:,1] *= -1
+                ymax = X[:, 1].max()
+                idx = X[:, 1] <= -ymax
+                tmp = X[idx, :]
+                tmp[:, 1] *= -1
                 X = np.vstack((X, tmp))
                 region = np.hstack((region, region[idx]))
 
             cells.append(X)
             regions.append(region)
         return np.vstack(cells), np.hstack(regions)
+
+    def plot_correction(self, density_param_id=1, cut_distance=15):
+        # bin_width = (DensityParameters() & dict(density_param_id=density_param_id)).fetch1['bin_width']
+        rel_correction = (self.OrthoOverlapDensity() & dict(density_param_id=density_param_id)) \
+                         * CellNameTranslation().project(cell_type_from='shan', pre='paper', un1='xiaolong') \
+                         * CellNameTranslation().project(cell_type_to='shan', post='paper', un2='xiaolong')
+        rel_prob = PaperConnectivity() * CellNameTranslation().project(cell_type_from='xiaolong', pre='paper',
+                                                                       un1='shan') \
+                   * CellNameTranslation().project(cell_type_to='xiaolong', post='paper', un2='shan')
+
+        df_correction = pd.DataFrame(rel_correction.fetch())
+        df_paper = pd.DataFrame(rel_prob.fetch())
+
+        P = df_paper.groupby(['post', 'pre']).agg({'p': lambda x: x})
+
+        gr = df_correction.groupby(['post', 'pre'])
+
+        D0 = gr.agg({'p': lambda x: np.mean(x, axis=0).sum() * 2})
+
+        D = gr.agg({'p': lambda x: np.mean(x, axis=0)[1:].sum()})  # TODO: replace 1: at some point
+        Q = 1 - D / D0
+
+        P2 = (P / Q).fillna(0)
+        P2 = P2.unstack()
+
+        P = P.unstack()
+
+        plot_connections(P.as_matrix(), P.index)
+        plot_connections(P2.as_matrix(), P2.index)
+        # ----------------------------------
+        # TODO: Remove this later
+        from IPython import embed
+        embed()
+        exit()
+        # ----------------------------------
+
+
+        # for group in (self & dict(density_param_id=density_param_id)).fetch.as_dict:
+        #     d,p = (OverlapGroup.OrthoOverlapDensity() & group).fetch['d','p']
+        #     #----------------------------------
+        #
+        #     # TODO: Remove this later
+        #     from IPython import embed
+        #     embed()
+        #     exit()
+        #     #----------------------------------
--- python/cortical_map/utils.py
+++ python/cortical_map/utils.py
@@ -1,6 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+from matplotlib.colors import Normalize
+from matplotlib.colorbar import ColorbarBase
+from matplotlib.cm import ScalarMappable
+from matplotlib.pyplot import fill, cm, Rectangle
+import os
 
 def rasterize(node_coords, connected_pairs, region, resolution=1):
     """
@@ -51,15 +57,24 @@ def spin(X, theta):
     return np.dot(X, R.T)
 
 
-def plot_cells(X, Y, delta, param1, param2):
-    fig = plt.figure(figsize=(12, 12), dpi=400)
-    ax = fig.add_subplot(1, 1, 1, projection='3d')
+def plot_cells(X, Y, delta, param1, param2, threed=False):
+
     Y = Y+delta
-    ax.plot(X[:, 0], X[:, 1], X[:, 2], '.', ms=.5, **param1)
-    ax.plot(Y[:, 0], Y[:, 1], Y[:, 2], '.', ms=.5, **param2)
+    sns.set_style('whitegrid')
+    fig = plt.figure(figsize=(12, 12), dpi=400)
+
+    if threed:
+        ax = fig.add_subplot(1, 1, 1, projection='3d')
+        ax.plot(X[:, 0], X[:, 1], X[:, 2], '.', ms=.5, **param1)
+        ax.plot(Y[:, 0], Y[:, 1], Y[:, 2], '.', ms=.5, **param2)
+        ax.set_zlabel(r'relative cortical depth [$\mu$m]')
+    else:
+        ax = fig.add_subplot(1, 1, 1)
+        ax.plot(X[:, 1], X[:, 2], '.', ms=.5, **param1)
+        ax.plot(Y[:, 1], Y[:, 2], '.', ms=.5, **param2)
+
     ax.set_xlabel(r'parallel to cut [$\mu$m]')
     ax.set_ylabel(r'perpendicular to cut [$\mu$m]')
-    ax.set_zlabel(r'relative cortical depth [$\mu$m]')
 
     lgnd = ax.legend()
     for h in lgnd.legendHandles:
@@ -88,3 +103,126 @@ def compute_overlap_density(X, Y, bin_width, delta):
     H1 /= bin_width ** 3
     H2 /= bin_width ** 3
     return H1 * H2, E
+
+def extended_hinton(ax, V, C, vmax=None, cmin=None, cmax=None, cmap=None, matrix_style=False, alpha=1,
+                    enforce_box=False):
+    if cmap is None:
+        cmap = cm.jet
+
+    if vmax is None:  vmax = np.amax(np.abs(V))
+    if cmax is None:  cmax = np.amax(C)
+    if cmin is None:  cmin = np.amin(C)
+
+    cnorm = Normalize(vmin=cmin, vmax=cmax, clip=True)
+    cmapable = ScalarMappable(norm=cnorm, cmap=cmap)
+
+    if matrix_style:
+        V, C = V.T, C.T
+
+    ax.patch.set_facecolor([0, 0, 0, 0])
+
+    for (x, y), w in np.ndenumerate(V):
+        s = C[x, y]
+        color = cmap(cnorm(s))  # cmap(s / cmax)
+        size = np.abs(w / vmax)
+        rect = Rectangle([x - size / 2, y - size / 2], size, size,
+                         facecolor=color, edgecolor=color, alpha=alpha)
+        ret = ax.add_patch(rect)
+
+    if enforce_box:
+        ax.axis('tight')
+        try:
+            ax.set_aspect('equal', 'box')
+        except:
+            pass
+    #ax.autoscale_view()
+    #ax.invert_yaxis()
+    return cnorm
+
+def plot_connections(P, labels, vmax=None):
+    colors = ["black", "azure","apple","golden yellow",   "neon pink"]
+    cmap = sns.blend_palette(sns.xkcd_palette(colors), as_cmap=True)
+
+    sns.set_style('whitegrid')
+    sns.set_context('paper',rc={"lines.linewidth": 2, 'font.size':12, 'font.family':'Helvetica'})
+    fig = plt.figure(figsize=(4.6,4.6), dpi=300)
+    gs = plt.GridSpec(5,20)
+
+    ax = fig.add_subplot(gs[:,:17])
+    n = len(labels)
+
+    cnorm = extended_hinton(ax, P, P, matrix_style=True, cmap=cmap,
+                            vmax=vmax if vmax is not None else P.max(),
+                            cmin=0, cmax=P.max(),enforce_box=True)
+    ax.set_xticks(range(n))
+    ax.set_yticks(range(n))
+    ax.set_xticklabels(labels, rotation=90)
+    ax.set_yticklabels(labels)
+#     ax.set_title(r'full connection probability matrix', fontweight='bold')
+    ax.set_xlim((-1,n))
+    ax.set_ylim((-1,n))
+    ax.set_ylabel('postsynaptic')
+    ax.set_xlabel('presynaptic')
+    ax_color = fig.add_subplot(gs[1:4,18:])
+    cbar = ColorbarBase(ax_color, cmap=cmap, norm=cnorm)
+    fig.tight_layout()
+    ax.invert_yaxis()
+
+    return fig, {'matrix':ax, 'color':ax_color}
+
+def layer(name):
+    if 'L1' in name:
+        return 1
+    elif 'L23' in name:
+        if 'Pyr' in name:
+            return 3
+        else:
+            return 2
+    else:
+        if 'Pyr' in name:
+            return 5
+        else:
+            return 4
+
+
+def load_data(transpose=False, collapse=None, remove=None):
+    path = '/'.join(os.path.realpath(__file__).split('/')[:-1])
+    with open(path + '/files/matrix09112015.csv', 'r') as fid:
+        labels = [e.strip() for e in fid.readline().split(',')[1:]]
+        K, N = [], []
+        for l in fid.readlines():
+            K.append([list(map(float, e.strip().split('/')))[0] for e in l.split(',')[1:]])
+            N.append([list(map(float, e.strip().split('/')))[1] for e in l.split(',')[1:]])
+    layers = [layer(name) for name in labels]
+    K = np.asarray(K)
+    N = np.asarray(N)
+
+    if collapse is not None:
+        for k, v in collapse.items():
+            idx = list(sorted([labels.index(e) for e in v]))
+            i = idx[0]
+            labels[i] = k
+            K[i, :] = K[idx, :].sum(axis=0)
+            K[:, i] = K[:, idx].sum(axis=1)
+            N[i, :] = N[idx, :].sum(axis=0)
+            N[:, i] = N[:, idx].sum(axis=1)
+            K = np.delete(K, idx[1:], axis=0)
+            K = np.delete(K, idx[1:], axis=1)
+            N = np.delete(N, idx[1:], axis=0)
+            N = np.delete(N, idx[1:], axis=1)
+            labels = [labels[i] for i in range(len(labels)) if i not in idx[1:]]
+            layers = [layers[i] for i in range(len(layers)) if i not in idx[1:]]
+    if remove is not None:
+        idx = list(sorted([labels.index(e) for e in remove]))
+        K = np.delete(K, idx, axis=0)
+        K = np.delete(K, idx, axis=1)
+        N = np.delete(N, idx, axis=0)
+        N = np.delete(N, idx, axis=1)
+        labels = [labels[i] for i in range(len(labels)) if i not in idx]
+        layers = [layers[i] for i in range(len(layers)) if i not in idx]
+
+    if transpose:
+        return labels, layers, K.T, N.T
+    else:
+        return labels, layers, K, N
+
